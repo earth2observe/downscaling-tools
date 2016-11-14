e98058edb58ab96ba1e94de58166193221d5b561
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-11-14 14:35:20 +0100

    mock order

--- doc/conf.py
+++ doc/conf.py
@@ -148,6 +148,7 @@ pygments_style = 'sphinx'
 # of the sidebar.
 html_logo = "_static/logo.png"
 
+
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.

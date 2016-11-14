201e9a5d3d3393b4611be05356bb7edce3f8aae6
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-11-14 13:58:19 +0100

    mock order

--- examples/getvar/examplerun1.bat
+++ examples/getvar/examplerun1.bat
@@ -1,6 +1,5 @@
 @echo off
-echo Get precipitation and Temperature for the first 15 days of the first month of 2010 and 
-echo resamples the data to a local dem using linear interpolation.
+
 pause Press enter to tun...
 ..\..\e2o_dstools-64-bit\e2o_getvar.exe -I examplerun1.ini
 echo results can be found in the output directory
